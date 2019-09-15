243f0249f8008fc2e62dc3e043e9e9a10c0b0c14
Guru Prasath <54740756+gguruprasath@users.noreply.github.com>
2019-09-15 20:29:41 +0530

    Take user input to add a new quote

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,16 @@ import random
 def primary():
   #print("Keep it logically awesome.")
   
+  f = open("quotes.txt","a")
+  wrt = input("Enter any novel quote:")
+  f.write("\n" + wrt)
+  f.close()
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   last = len(quotes)-1
   rnd = random.randint(0, last)
-  print(quotes[rnd],end="")
+  print("Quotes:\n"+quotes[rnd],end="")
   rnd = random.randint(0, last)
   print(quotes[rnd],end="")
 
